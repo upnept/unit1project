52c31380a4ea6601e6cc70922d867a56478ac4be
upnept <chris.mejia@southeasttech.edu>
2026-06-06 17:28:44 -0500

    nrml err

---
+++ error.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Movie Manager</title>
+        <link rel="stylesheet" type="text/css" href="main.css"/>
+    </head>
+
+    <body>
+        <header><h1>Movie Manager</h1></header>
+
+        <main>
+            <h1>Error</h1>
+            <p>&nbsp;</p>
+        </main>
+
+        <footer>
+            <p>&copy; Movie Manager</p>
+        </footer>
+    </body>
+</html>
