c08337605d19c466804647d3d365b2e5bc1f2ddb
upnept <chris.mejia@southeasttech.edu>
2026-06-06 17:53:58 -0500

    movies page

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ $statement->closeCursor();
 
 <!DOCTYPE html>
 
+<!DOCTYPE html>
+
 <html>
     <head>
         <title>Movie Manager</title>
@@ -20,6 +22,33 @@ $statement->closeCursor();
 
     <body>
         <header><h1>Movie Manager</h1></header>
+
+        <section>
+            <!-- display movies -->
+            <table>
+                <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Rating</th>
+                    <th>Rating</th>
+                </tr>
+
+                <?php foreach ($movies as $movie) : ?>
+                <tr>
+                    <td><?php echo $movie['title']; ?></td>
+                    <td><?php echo $movie['genre']; ?></td>
+                    <td><?php echo $movie['rating']; ?></td>
+                    <td><?php echo $movie['year']; ?></td>
+                    <td><form action="delete_movie.php" method="post">
+                        <input type="hidden" name="movie_id"
+                            value="<?php echo $movies['movieID']; ?>">
+                        <input type="submit" value="Delete">
+                    </form></td>
+                </tr>
+                <?php endforeach; ?>
+            </table>
+            <p><a href="add_movie_form.php">Add Movie</a></p>
+        </section>
 
         <footer>
             <p>&copy; Movie Manager</p>
